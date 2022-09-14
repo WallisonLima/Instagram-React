9ec9f5280373172fd083316ef8ecb59bca4bf250
WallisonLima <wallisonlimaoliveira@gmail.com>
2022-09-14 18:43:56 -0300

    estilizando container left

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
+import './styles.css'
+
+import instagramLogo from '../../assets/instagram-logo.png'
+
+
 export const Dashboard = () => {
     return (
         <div className="container">
-            <div>Olá a principal</div>
+            <div className='container-left'>
+                <img src={instagramLogo} alt="Logo do instagram" />
+            </div>
+            <div className='container-center'>centro</div>
+            <div className='container-rigth'>lado direito</div>
         </div>
     )
 }
